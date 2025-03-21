13eda2babf87eed29c0333422f82b7a39553069c
RayZeg <dzgamingmaster@gmail.com>
2025-03-21 02:31:11 +0100

    changed success text color to white and centered... i think didnt check

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,6 +1,6 @@
 export default function Success() {
   return (
-    <div>
+    <div className="text-white flex flex-col justify-center items-center h-dvh">
       <h1>Success Confirmation email has been sent</h1>
       <p>
         well not really psq too lazy to set up smtp on my end but you get the
